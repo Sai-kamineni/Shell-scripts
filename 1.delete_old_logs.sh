e7e538f1b7826b4372f8e23a48b9eb5ddc72ab88
Sai-kamineni <saikumar.dba10@gmail.com>
2026-05-04 21:20:37 +0530

    shell-scripts

--- 1.delete_old_logs.sh
+++ 1.delete_old_logs.sh
@@ -14,12 +14,22 @@ else
     exit 1
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-echo "Files: $FILES"
 
-# dont use line, it is reserverd word
-while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-do
-    echo "Deleting file: $file"
-    rm -rf $file
-done <<< $FILES
+echo -e "$G Deleting files older thn 14 days $N"
+
+#find and delete the files
+
+find "${SOURCE_DIR}" -type f -name "*.log" -mtime +14 rm -rf {} \;
+
+
+
+
+# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+# echo "Files: $FILES"
+
+# # dont use line, it is reserverd word
+# while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+# do
+#     echo "Deleting file: $file"
+#     rm -rf $file
+# done <<< $FILES
--- 3.Disu_usage.sh
+++ 3.Disu_usage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects, it is usually 75
-
+EMAIL="mail.com"
 
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
@@ -10,6 +10,12 @@ do
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+        # echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+        alert= "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+
+        echo "alert"
+
+        #send mail
+        echo "$alert" | mail -s "Disk alert on server" $EMAIL 
     fi
 done <<< $DISK_USAGE
